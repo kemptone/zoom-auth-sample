461b0c901d1c84ee8ab7d8aa12a265a86e789f59
justin kempton <justin@talentsky.com>
2023-05-18 15:16:26 -0700

    debugging

--- api/authenticate.js
+++ api/authenticate.js
@@ -12,21 +12,46 @@ export default function handler(req, res) {
 
   const oHeader = { alg: 'HS256', typ: 'JWT' }
 
-  const oPayload = {
-    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
-    mn: req.body.meetingNumber,
-    role: req.body.role,
-    iat: iat,
-    exp: exp,
-    appKey: process.env.ZOOM_MEETING_SDK_KEY,
-    tokenExp: iat + 60 * 60 * 2
-  }
-
-  const sHeader = JSON.stringify(oHeader)
-  const sPayload = JSON.stringify(oPayload)
-  const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log("////////////////////")
+  // console.log(req.body)
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log("||||||||||||||||||||")
+  // console.log(process.env.ZOOM_MEETING_SDK_KEY)
+  // console.log(process.env.ZOOM_MEETING_SDK_SECRET)
 
   res.json({
-    signature: signature
+    message: 'Hello World!'
+    , SDK : process.env.ZOOM_MEETING_SDK_KEY
+    , body : req.body
   })
+
+
+  // const oPayload = {
+  //   sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+  //   mn: req.body.meetingNumber,
+  //   role: req.body.role,
+  //   iat: iat,
+  //   exp: exp,
+  //   appKey: process.env.ZOOM_MEETING_SDK_KEY,
+  //   tokenExp: iat + 60 * 60 * 2
+  // }
+
+  // const sHeader = JSON.stringify(oHeader)
+  // const sPayload = JSON.stringify(oPayload)
+  // const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
+
+  // res.json({
+  //   signature: signature
+  // })
 }
